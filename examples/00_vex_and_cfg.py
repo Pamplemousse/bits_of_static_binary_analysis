2897498f237530161efbc4024ca6a15050a41cda
Pamplemousse <xav.maso@gmail.com>
2020-10-01 15:46:05 -0700

    Reorganise content of first example for better flow
    Signed-off-by: Pamplemousse <xav.maso@gmail.com>

--- examples/00_vex_and_cfg.py
+++ examples/00_vex_and_cfg.py
@@ -2,18 +2,19 @@ from angr import Project
 
 
 project = Project('build/vex_and_cfg', auto_load_libs=False)
-cfg = project.analyses.CFGFast(normalize=True, data_references=True)
+architecture = project.arch
+cfg = project.analyses.CFGFast()
 
 main_function = project.kb.functions.function(name='main')
 
 # `main` contains several basic blocks.
 blocks_of_main = list(main_function.blocks)
 
+# VEX of the first block of the `main` function.
+main_vex = blocks_of_main[0].vex
+
 # Get the first node (basic block) of `main` from the CFG.
 main_entry_node = cfg.model.get_any_node(main_function.addr)
-
-# VEX of the first block of the function `main`.
-main_vex = main_entry_node.block.vex
 
 # ???
 mystery_node = main_entry_node.successors[0]
